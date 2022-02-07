e1b9f48857cc4711d76cce7630316928d5e65939
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-02-07 22:49:37 +0100

    normal dist fix responsive, add reset button, add cache

--- R/normal-distribution/app.R
+++ R/normal-distribution/app.R
@@ -1,45 +1,54 @@
-
-
-
 library(shiny)
+library(miniUI)
 library(tidyverse)
 
-source("../functions/helper_elements.R")
+options(shiny.autoreload = TRUE)
 
-ui <- shiny::fillPage(shiny::fillCol(
-    shiny::plotOutput("distPlot", width = "100%", height = "100%"),
-    shiny::fillRow(
-        tags$div(
-            shiny::sliderInput("mean",
-                               "Mean", -10,
-                               10,
-                               0,
-                               1,
-                               width = "100%"),
-            style = "margin: 1em; height: min-content;"
-        ),
-        tags$div(
-            shiny::sliderInput("sd",
-                               "Standard deviation",
-                               0.1,
-                               10,
-                               1,
-                               .1,
-                               width = "100%"),
-            style = "margin: 1em; height: min-content;"
-        )
+PRECISION = .01
+RANGE = c(-10, 10)
+
+ui <- miniPage(
+    miniContentPanel(plotOutput(
+        "distPlot", width = "100%", height = "100%"
+    )),
+    miniButtonBlock(
+        style = "justify-content: space-between; gap: 1em;",
+        sliderInput("mean",
+                    "Mean", -10,
+                    10,
+                    0,
+                    1,
+                    width = "100%"),
+        sliderInput("sd",
+                    "Standard deviation",
+                    0.1,
+                    10,
+                    1,
+                    .1,
+                    width = "100%"),
+        actionButton("reset", "Reset to standard normal", icon = icon("undo-alt"), class = "btn btn-primary", style = "height: min-content; margin: auto 0;")
     )
-))
+)
 
-server <- function(input, output) {
+
+server <- function(input, output, session) {
+    onResetSN <- observe({
+        updateSliderInput(session, "mean", value = 0)
+        updateSliderInput(session, "sd", value = 1)
+    }) %>% bindEvent(input$reset)
+
+    data <- reactive({
+        tibble(
+            x = seq(RANGE[1], RANGE[2], PRECISION),
+            dx = dnorm(x, input$mean, input$sd)
+        )
+    }) %>% bindCache(input$mean, input$sd)
+
     output$distPlot <- renderPlot({
-        # sample data
-        data <- tibble(x = seq(-10, 10, .1),
-                       dx = dnorm(x, input$mean, input$sd))
-
-        # make a plot
-        ggplot(data, aes(x, dx)) + geom_line() + coord_cartesian(ylim = c(0, max(data$dx, .4)))
-    })
+        ggplot(data(), aes(x, dx)) +
+            geom_line() +
+            coord_cartesian(ylim = c(0, max(data()$dx, .4)))
+    }) %>% bindCache(input$mean, input$sd)
 }
 
 shinyApp(ui = ui, server = server)
