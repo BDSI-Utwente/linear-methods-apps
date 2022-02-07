e093977566d605abf6d4b8cf7e8bc88d009cc7c2
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-02-07 22:32:34 +0100

    correlation: fix responsive layout, simplify code

--- R/correlation/app.R
+++ R/correlation/app.R
@@ -1,64 +1,53 @@
 require(tidyverse)
+require(miniUI)
 require(shiny)
 require(shinyjs)
 require(plotly)
 
+# options(shiny.autoreload = TRUE)
+
 INITIAL_R = .5
 NUM_OBSERVATIONS = 300
+SEED = 1234
 LIMITS = c(-4, 4)
 
-# define function for sampling data with a given r ------------------------
 # based on https://stats.stackexchange.com/questions/522454/how-to-construct-simulate-data-that-will-have-a-given-coefficient-of-determina/522470#522470
-sample_linear_with_cor <- function(r, x, b0 = 0, b1 = 1) {
-    r_square <- r * r
-    n <- length(x)
-    y_hat <- b0 + b1 * x
-
-    ssr <- sum((y_hat - mean(y_hat)) ^ 2)
-    e <- rnorm(n)
-    e <- resid(lm(e ~ x))
-    e <- e * sqrt((1 - r_square) / r_square * ssr / (sum(e ^ 2)))
-
-    y <- y_hat + e
-    y
-}
 
 
 # Define UI for application that draws a histogram
-ui <- fillPage(useShinyjs(),
-               fillCol(
-                   plotlyOutput("corPlot", height = "100%"),
-                   fillRow(
-                       sliderInput(
-                           "r",
-                           "Correlation",
-                           0,
-                           1,
-                           value = INITIAL_R,
-                           0.01,
-                           width = "100%"
-                       ),
+ui <- miniPage(
+    useShinyjs(),
+    miniContentPanel(plotOutput("corPlot", height = "100%")),
+    miniButtonBlock(
+        style = "justify-content: space-around; align-items: center; gap: 1em;",
 
-                       tags$div(
-                           tags$label("Linear fit", class = "control-label"),
-                           checkboxInput("linear", "Show linear fit?", FALSE, width = "100%"),
-                           checkboxInput("se", "Show uncertainty?", FALSE, width = "100%"),
-                       ),
-                       style = "gap: 1em;"
-                   )
-               ), padding = "1em")
+        sliderInput("r",
+                    "Correlation",
+                    0,
+                    1,
+                    value = INITIAL_R,
+                    0.01),
+
+        checkboxInput("linear", "Show linear fit?", FALSE),
+        checkboxInput("se", "Show uncertainty?", FALSE)
+
+    )
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    set.seed(SEED)
     x <- rnorm(NUM_OBSERVATIONS)
-    y <- reactiveVal(sample_linear_with_cor(INITIAL_R, x))
+    ssr <- sum((x - mean(x)) ^ 2)
+    e <- lm(e ~ x, data = tibble( e = rnorm(NUM_OBSERVATIONS), x )) %>% resid()
+    y <- reactive({
+        r2 <- input$r ^ 2
+        error <- e * sqrt((1 - r2) / r2 * ssr / sum(e ^ 2))
+        x + error
+    }) %>% bindEvent(input$r)
 
     observe({
-        y(sample_linear_with_cor(input$r, x))
-    })
-
-    observe({
-        if(input$linear){
+        if (input$linear) {
             enable("se")
         } else {
             disable("se")
@@ -66,16 +55,17 @@ server <- function(input, output) {
         }
     })
 
-    output$corPlot <- renderPlotly({
+    output$corPlot <- renderPlot({
         plot <- ggplot(tibble(x, y = y()), aes(x, y)) +
             geom_point() +
             coord_cartesian(xlim = LIMITS, ylim = LIMITS)
         if (input$linear) {
             plot <- plot + geom_smooth(method = "lm", se = input$se)
         }
-        plot %>% plotly::ggplotly()
-    })
+        plot # %>% plotly::ggplotly()
+    }) %>% bindCache(input$r, input$linear, input$se)
 }
 
 # Run the application
+
 shinyApp(ui = ui, server = server)
